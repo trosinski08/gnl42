80074ad9a0a446232d554fd786dd592de9b0d70c
trosinski08 <trosinski08@gmail.com>
2024-06-27 21:21:18 +0200

    d

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: trosinsk <trosinsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 06:56:35 by trosinsk          #+#    #+#             */
-/*   Updated: 2024/06/25 11:30:21 by codespace        ###   ########.fr       */
+/*   Updated: 2024/06/27 21:19:39 by trosinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,17 @@
 #  define OPEN_MAX 1024
 # endif
 
+# include "../../libft/include/libft.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(char *s, int c);
-size_t	ft_strlen(char *str);
-char	*ft_substr(char *s, unsigned int start, size_t len);
-char	*ft_strdup(char *s);
+// char	*ft_strjoin(char *s1, char *s2);
+// char	*ft_strchr(char *s, int c);
+// size_t	ft_strlen(char *str);
+// char	*ft_substr(char *s, unsigned int start, size_t len);
+// char	*ft_strdup(char *s);
 char	*line_cleaner(char *str);
 char	*line_maker(char *str);
 
