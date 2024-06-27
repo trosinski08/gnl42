51eea4d881f2a8b0b0e25d6f5e963aa405dba1c4
trosinski08 <trosinski08@gmail.com>
2024-06-27 20:41:19 +0200

    asassa

---
+++ Makefile
@@ -0,0 +1,38 @@
+cc -Wall -Wextra -Werror -D BUFFER_SIZE=32 main.c ./bonus/get_next_line_bonus.c ./mini_libft/get_next_line_utils.c -o get_next_line_test
+NAME = gnl
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+BUFFER_SIZE = 32
+LIBFT = ./libft
+HEADERS = -I ./libft -I ./include
+LIBS = -L $(LIBFT) -lft
+SRC = ./mandatory/get_next_line.c
+# main.c
+BNS = ./bonus/get_next_line_bonus.c
+OBJS = $(SRC:.c=.o)
+BNO = $(BNS:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(MAKE) -sC $(LIBFT)
+	$(CC) $(CFLAGS) $(HEADERS) $(OBJS) $(LIBS) -o $(NAME)
+
+bonus: $(BNO)
+	$(MAKE) -sC $(LIBFT)
+	$(CC) $(CFLAGS) $(HEADERS) $(BNO) $(LIBS) -o $(NAME)
+
+%.o: %.c
+	$(CC) $(CFLAGS) $(HEADERS) -c $< -o $@
+
+clean:
+	$(MAKE) -sC $(LIBFT) clean
+	rm -f $(OBJS) $(BNO)
+
+fclean: clean
+	$(MAKE) -sC $(LIBFT) fclean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re bonus
--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trosinsk <trosinsk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 19:48:25 by trosinsk          #+#    #+#             */
+/*   Updated: 2024/06/27 19:48:26 by trosinsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,22 +18,24 @@
 //cc -Wall -Wextra -Werror -D BUFFER_SIZE=32 main.c ./bonus/get_next_line_bonus.c ./mini_libft/get_next_line_utils.c -o get_next_line_test
 //./get_next_line_test ./mandatory/get_next_line.c
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int fd;
-    char *line;
+	int		fd;
+	char	*line;
 
-    if (argc != 2)
-        return (printf("File: %s\n", argv[0]), 1);
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-        return (perror("Błąd przy otwieraniu pliku"), 1);
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
-    if (close(fd) == -1)
-        return (perror("Błąd przy zamykaniu pliku"), 1);
-    return (0);
+	if (argc != 2)
+		return (printf("File: %s\n", argv[0]), 1);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (perror("Błąd przy otwieraniu pliku"), 1);
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	if (close(fd) == -1)
+		return (perror("Błąd przy zamykaniu pliku"), 1);
+	return (0);
 }
